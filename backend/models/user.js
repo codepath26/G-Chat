0708828bb1448a94ae320e9d96ec931e28e824a2
parth thakor <parththakor2610@gmail.com>
2024-02-12 22:45:30 +0530

    adding the  user authentication and register apis

---
+++ backend/models/user.js
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    pic: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("User", userSchema);
+export default User;
---
+++ backend/routes/user.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { getUsers, loginHandler, signupHandler } from "../controllers/user.js";
+import authenticateUser from "../middleware/authenticate.js";
+
+const router = Router();
+
+router.post("/signup", signupHandler);
+router.post("/login", loginHandler);
+router.get("/user", authenticateUser, getUsers);
+
+export default router;
